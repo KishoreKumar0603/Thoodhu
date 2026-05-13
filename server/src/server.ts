e1f192ad5fb9bf8ddaf499f882fe9789ab09bea6
KishoreKumar0603 <kishorekumars5643@gmail.com>
2026-05-14 00:17:34 +0530

    project setup done

---
+++ server/src/server.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(helmet());
+
+app.get("/", (_, res) => {
+  res.send("Thoodhu API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running on port ${PORT}`
+  );
+});
